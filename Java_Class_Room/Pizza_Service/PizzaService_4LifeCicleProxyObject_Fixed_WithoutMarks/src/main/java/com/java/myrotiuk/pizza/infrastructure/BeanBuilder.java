2f59d6bc3cff66ec53720fabf92614124a9d4fb4
IvanMyrotiuk <Ivan_Myrotiuk@epam.com>
2016-04-05 16:17:08 +0300

    without marks pizza service Life cicle(Proxy...)

---
+++ Java_Class_Room/Pizza_Service/PizzaService_4LifeCicleProxyObject_Fixed_WithoutMarks/src/main/java/com/java/myrotiuk/pizza/infrastructure/BeanBuilder.java
@@ -0,0 +1,99 @@
+package com.java.myrotiuk.pizza.infrastructure;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import com.java.myrotiuk.pizza.repository.PizzaRepository;
+import com.java.myrotiuk.pizza.repository.inmempizza.InMemPizzaRepository;
+
+public class BeanBuilder {
+
+	private final Class<?> clazz;
+	private ApplicationContext applicationContext;
+	private Object bean;
+
+	public BeanBuilder(Class<?> clazz, ApplicationContext applicationContext) {
+		this.clazz = clazz;
+		this.applicationContext = applicationContext;
+	}
+
+	protected Object createBean()
+			throws InstantiationException, IllegalAccessException, Exception, InvocationTargetException {
+		Constructor<?> constructor = clazz.getConstructors()[0];
+
+		if (constructor.getParameterCount() == 0) {
+			bean = clazz.newInstance();
+		} else {
+			bean = createNewInstanceWithParams(constructor);
+		}
+		return bean;
+	}
+
+	private Object createNewInstanceWithParams(Constructor<?> constructor)
+			throws Exception, InstantiationException, IllegalAccessException, InvocationTargetException {
+		Object bean;
+		Object[] paramBeans = getParams(constructor);
+		bean = constructor.newInstance(paramBeans);
+		return bean;
+	}
+
+	private Object[] getParams(Constructor<?> constructor) throws Exception {
+		Class<?>[] paramTypes = constructor.getParameterTypes();
+		Object[] paramBeans = new Object[paramTypes.length];
+		for (int i = 0; i < paramTypes.length; i++) {
+			getParams(paramTypes, paramBeans, i);
+		}
+		return paramBeans;
+	}
+
+	private void getParams(Class<?>[] paramTypes, Object[] paramBeans, int i) throws Exception {
+		String paramName = getBeanNameByType(paramTypes[i]);
+		paramBeans[i] = applicationContext.getBean(paramName);// to lower//cicle
+																// case
+	}
+
+	private String getBeanNameByType(Class<?> paramTypes) {
+		String paramTypeName = paramTypes.getSimpleName();
+		System.out.println(paramTypeName);
+		String paramName = changeFirstLetterToLowerCase(paramTypeName);
+		return paramName;
+	}
+
+	private String changeFirstLetterToLowerCase(String paramTypeName) {
+		String paramName = Character.toLowerCase(paramTypeName.charAt(0)) + paramTypeName.substring(1);
+		return paramName;
+	}
+
+	public void createBeanProxy() {
+		bean = MyTimeProxy.getProxyInstance(bean);
+	}
+	
+	public void callPostConstructMethod() throws Exception {
+		Method[] methods = clazz.getMethods();
+		for(Method m : methods){
+			if(m.getAnnotation(PostConstruct.class) != null){
+				InvocationHandler ih = Proxy.getInvocationHandler(bean);
+				Object original = ((MyTimeProxy)ih).getUnwrappedInvocationHandler();
+				m.invoke(original);
+			}
+		}
+	}
+
+	public void callInitMethod() throws Exception{
+		Method[] methods = clazz.getMethods();
+		for(Method m: methods){
+			if(m.getName().equals("init")){
+				InvocationHandler ih = Proxy.getInvocationHandler(bean);
+				Object original = ((MyTimeProxy)ih).getUnwrappedInvocationHandler();
+				m.invoke(original);
+			}
+		}
+	}
+
+	public Object build() {
+		return bean;
+	}
+}
